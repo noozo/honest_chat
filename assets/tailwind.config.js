7167930430a87f0e5c2eefcaca260658e4605624
Pedro Assunção <assuncas@gmail.com>
2021-10-28 15:33:54 +0100

    Adding credo and sobelow

---
+++ assets/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './js/**/*.js',
+    '../lib/*_web/**/*.*ex'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :honest_chat, HonestChatWeb.Endpoint,
   secret_key_base: "PPuAq4zKNNIHyiBn3NrIUPmx9e3hrfRAx2Ch02m7Pxzer+RPlzc+h3Ozxpbl/FWa",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
