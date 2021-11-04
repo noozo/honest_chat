f3eb17c0cea13ec577d82aec420c3a757901f1c9
Pedro Assunção <assuncas@gmail.com>
2021-11-04 15:59:12 +0000

    Room joining and displaying

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 120,
   subdirectories: ["priv/*/migrations"]
 ]
